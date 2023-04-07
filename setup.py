633689d161ab6cedecec7a45a326094c60ade2da
Jayesh Katade <59976614+jayesh-07@users.noreply.github.com>
2023-04-07 13:50:44 -0400

    Initial commit Created a Readme doc for project details. Setup.py is a file for setting up the package. All package related modules and functions are inside intelliprocess. __init__ file inisde intelliprocess is kept empty on purpose.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='containers',
+      version='0.1',
+      description='Example container data structures for DS 5010',
+      url='http://github.com/kuwisdelu/containers',
+      author='Kylie A. Bemis',
+      author_email='k.bemis@northeastern.edu',
+      license='Artistic 2.0',
+      packages=['containers'],
+      zip_safe=False)
