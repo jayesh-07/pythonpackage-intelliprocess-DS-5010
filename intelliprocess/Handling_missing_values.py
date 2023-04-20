255577478683970d6c5c10fc8337ca8dceedbc54
Jayesh Katade <59976614+jayesh-07@users.noreply.github.com>
2023-04-20 01:01:47 -0400

    Handling missing values
    Added a unit test for all data types also JUST KEPT THE MEAN STRAT as the strategy for value replacement as other methods were increasing the complexity, The test cases are also ran keeping the mean strategy in mind

--- intelliprocess/Handling_missing_values.py
+++ intelliprocess/Handling_missing_values.py
@@ -1,15 +1,13 @@
 import pandas as pd
 import numpy as np
 
-def fill_missing_values(df, method='mean'):
+def fill_missing_values(df):
     """
-    Iterates through the columns of a DataFrame, checks for NaN values, and replaces them with either the mean or mode
-    of the column, depending on the user's choice. Handles all data types including numeric, boolean, categorical,
-    string, datetime, and timedelta.
+    Iterates through the columns of a DataFrame, checks for NaN values, and replaces them with the mean of the column.
+    Handles all data types including numeric, boolean, categorical, string, datetime, and timedelta.
     
     Parameters:
     df (pandas.DataFrame): The DataFrame to be filled with missing values.
-    method (str): The method used to fill the missing values. Can be 'mean' or 'mode'. Defaults to 'mean'.
     
     Returns:
     pandas.DataFrame: The DataFrame with missing values filled.
@@ -21,32 +19,19 @@ def fill_missing_values(df, method='mean'):
         # Check the data type of the column
         dtype = str(df[column].dtype)
         
-        # If the column is numeric
-        if dtype.startswith('float') or dtype.startswith('int'):
+        # If the column is numeric or boolean
+        if dtype.startswith('float') or dtype.startswith('int') or dtype == 'bool':
             
-            # If the user selected 'mean', fill missing values with the mean of the column
-            if method == 'mean':
-                mean = df[column].mean()
-                df[column] = df[column].fillna(mean)
-            
-            # If the user selected 'mode', fill missing values with the mode of the column
-            elif method == 'mode':
-                mode = df[column].mode()[0]
-                df[column] = df[column].fillna(mode)
-        
-        # If the column is boolean
-        elif dtype == 'bool':
-            
-            # Fill missing values with the mode of the column
-            mode = df[column].mode()[0]
-            df[column] = df[column].fillna(mode)
-        
+            # Fill missing values with the mean of the column
+            mean = df[column].mean()
+            df[column] = df[column].fillna(mean)
+                
         # If the column is categorical
         elif dtype == 'category':
             
-            # Fill missing values with the mode of the column
-            mode = df[column].mode()[0]
-            df[column] = df[column].fillna(mode)
+            # Fill missing values with the mean of the column
+            mean = df[column].mean()
+            df[column] = df[column].fillna(mean)
         
         # If the column is a string or object
         elif dtype.startswith('str') or dtype.startswith('object'):
@@ -58,15 +43,15 @@ def fill_missing_values(df, method='mean'):
         # If the column is a datetime or timedelta
         elif dtype.startswith('datetime') or dtype.startswith('timedelta'):
             
-            # Fill missing values with the median of the column
-            median = df[column].median()
-            df[column] = df[column].fillna(median)
+            # Fill missing values with the mean of the column
+            mean = df[column].mean()
+            df[column] = df[column].fillna(mean)
             
         # If the column is any other data type
         else:
             
-            # Fill missing values with the mode of the column
-            mode = df[column].mode()[0]
-            df[column] = df[column].fillna(mode)
+            # Fill missing values with the mean of the column
+            mean = df[column].mean()
+            df[column] = df[column].fillna(mean)
     
     return df
