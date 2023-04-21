15810be45fbfd75ee876115ffa65e5c519c3d02d
taygoodwin <124896968+taygoodwin@users.noreply.github.com>
2023-04-21 17:03:22 -0400

    Scaling, Skewness and Outlier Detection update
    update. All but two functions have been mildly tested. The two that still need work at the scaling_log and select_x fcns. The scaling_box_cox fcn depends upon the select_x fcn, and will need to be revisited to ensure good scaling and plotting once  it runs.

--- intelliprocess/scaling_outliers_skewness.py
+++ intelliprocess/scaling_outliers_skewness.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -6,15 +5,29 @@ import seaborn as sns
 from scipy import stats
 import csv
 import math
+import os
 
 
 def column_names(data):
-    column_names = data.columns
-    column_names = column_names.tolist()
-    return column_names
+    """
+    Creates a list of column names for a given dataset.
+    :param data: a pandas data frame
+    :return:a list of the data's column names
+    """
+    columns_names = data.columns
+    columns_names = columns_names.tolist()
+    return columns_names
 
 
 def select_num(data_location):
+    """
+    Creates a subset of a given dataset, sampling
+    only the numeric variables.
+    :param data_location: string indicating file path
+    for a given dataset.
+    :return:a pandas data frame containing all columns
+    in which numeric values are identified.
+    """
     df_input = pd.read_csv(data_location)
     data = pd.DataFrame(df_input)
     numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
@@ -22,23 +35,42 @@ def select_num(data_location):
     return data_numeric
 
 
-def select_xy(data_numeric,x_index, y_index):
-    data_xy = data_numeric[x_index].combine(data_numeric[y_index])
-    return data_xy
-
-# Skewness and Histogram PLots:
-# Fisher-Pearson method of skew measure - replicate
-# include in report with graphs -- how to do?
-# Fix needed to 'skew' fcn
-# Notes: 'skew' keeps printing same value instead of multiple diff.
-def skew(data_numeric):
+def select_x(data_location, x_name):
     """
-
-    :param data_numeric:
+    Creates a subset of a single variable in a
+    given dataset, when provided the datasets
+    filepath and column name of the chosen variable.
+    :param data_location:
+    :param x_name: string indicating the name of
+    the requested column to subset.
     :return:
     """
+    df_input = pd.read_csv(data_location)
+    data = pd.DataFrame(df_input)
+    for i in range(len(column_names(data))):
+        if column_names(data)[i] == x_name:
+            data_x = data.values[i][0:len(data.values[i]) - 1]
+            data_x = pd.DataFrame(data_x)
+            return data_x
+        else:
+            print('Specified column not found')
+
+
+# Skewness and Histogram PLots:
+# Fisher-Pearson method of Skew Measure
+def skew(data_numeric):
+    """
+    Creates an associative array containing all column
+    names of a given dataset and their calculated
+    Fisher-Pearson skew value.
+    :param data_numeric: a dataset of only numerics
+    :return:skewness: a dictionary containing the
+    column names as keys and associated skewness numbers
+    as values.
+    """
     skewness = dict()
-    for i in range(len(column_names(data_numeric))):
+    data_cols = column_names(data_numeric)
+    for i in range(len(data_cols)):
         means = data_numeric.mean()
         mean = means.values
         modes = data_numeric.mode()
@@ -47,59 +79,61 @@ def skew(data_numeric):
         sd = sds.values
         skew_number_array = (mean[i] - mode[i]) / sd[i]
         skew_list = skew_number_array.tolist()
-        print(i)
-        key = [data_numeric.columns][i][0]
-        value = skew_list[i]
-        skewness[key] = value
-    return skewness
+        for j in range(len(data_cols)):
+            skewness[data_cols[j]] = skew_list[j]
+        return skewness
+
 
 def histogram_plots(data_numeric):
     """
-    Creates a 2x2 plot output of histograms with four varied binwidths
-    for each of the columns entered as input compared to the leftmost
-    variable.
-    :param
+    Creates a 2x2 plot of histograms, each utilizes one of
+    four varied binwidths (5, 10, 15, 20) and contain a
+    different colored histogram  for each of the columns
+    in a given numeric dataset.
+    :param data_numeric:  a dataset of only numerics
     :return: None
     """
     for i, binswidth in enumerate([5, 10, 15, 20]):
         axes = plt.subplot(2, 2, i + 1)
         axes.hist(data_numeric, bins=int(180 / binswidth))
 
-        axes.set_title('Histogram with Binwidth = %d' % binswidth, size = 10)
-        axes.set_xlabel('x_axis_label', size = 10)
-        axes.set_ylabel('y_axis_label', size = 10)
+        axes.set_title('Histogram with Binwidth = %d' % binswidth, size=10)
+        axes.set_xlabel('x_axis_label', size=10)
+        axes.set_ylabel('y_axis_label', size=10)
 
     plt.tight_layout()
     plt.show()
     return None
 
 
-# (2) scaling - logarithmic,
-# transformation techniques such as
-# box-cox, log transform, or rank-based
-def scaling_box_cox(data_numeric):
-        transformed_df, fitted_lambda = stats.boxcox(data_numeric.values)
+# Scaling/ Transforming Data:
+# Progressing - Need to check once 'select_x' fcn is complete
+def scaling_box_cox(x_column):
+    data_x = x_column.values
+    data_x = data_x.flatten()
 
-        for i in range(len(transformed_df)):
-            fig, axes = plt.subplots(1, 2)
+    print(data_x)
+    transformed, fitted_lambda = stats.boxcox(data_x)
 
-            sns.displot(data_numeric, hst = False, kde = True,
-                    kde_kws = {'shade': True, 'linewidth': 2},
-                    label = "Original Data", color ="black", ax = axes[0])
-            sns.displot(transformed_df, hst = False, kde = True,
-                    kde_kws = {'shade': True, 'linewidth': 2},
-                    label = "Normal", color ="black", ax = axes[1])
+    for i in range(len(transformed)):
+        fig, axes = plt.subplots(1, 2)
 
-        fig.set_figheight(5)
-        fig.set_figwidth(10)
-        print(f"Lambda value used for Transformation: {fitted_lambda}")
-        return fig
+        sns.displot(data_x, hst=False, kde=True,
+                    kde_kws={'shade': True, 'linewidth': 2},
+                    label="Original Data", color="black", ax=axes[0])
+        sns.displot(transformed, hst=False, kde=True,
+                    kde_kws={'shade': True, 'linewidth': 2},
+                    label="Normal", color="black", ax=axes[1])
+
+    fig.set_figheight(5)
+    fig.set_figwidth(10)
+    print(f"Lambda value used for Transformation: {fitted_lambda}")
+    return fig
 
 
-def construct_df(data_numeric_values):
-    pass
-
-
+# Log Transformation for a Given Column
+# Progressing - Incomplete
+# incorporate with scatterplot fcn (shane)
 def scaling_log(data_numeric):
     for i in range(len(data_numeric)):
         values = [data_numeric.values][i]
@@ -109,29 +143,41 @@ def scaling_log(data_numeric):
 
 
 # Outlier Detection and Removal
-# Enables IQR method for classification
+# Enables IQR method
 def outlier_removal_IQR_method(data_numeric):
     """
     Removes outliers based on inter quartile range
     and provides a csv file output.
     :param data_numeric: data frame of all numeric values.
-    :return: new csv file with outlier observations removed.
+    :return: updated_data: a new csv file with outlier
+    observations removed.
     """
     Q3 = data_numeric.quantile(0.75)
     Q1 = data_numeric.quantile(0.25)
-    IQR = (Q3-Q1)
+    IQR = (Q3 - Q1)
 
     updated_data = data_numeric[~(
-            (data_numeric < (Q1-1.5*IQR)) | (data_numeric > (Q3+1.5*IQR)))]
+            (data_numeric < (Q1 - 1.5 * IQR)) | (data_numeric > (Q3 + 1.5 * IQR)))]
 
     updated_data.to_csv('data_removed.csv')
     return updated_data
 
 
 if __name__ == "__main__":
+    # Progressing
+    print(scaling_box_cox(select_x('/Users/taylorgoodwin/Documents/GitHub/'
+                                   'pythonpackage-intelliprocess-DS-5010/'
+                                   'intelliprocess/Testing_Datasets/iris.csv', 'sepal_width')))
+    # Works
     print(skew(select_num('/Users/taylorgoodwin/Documents/GitHub/'
-                                     'pythonpackage-intelliprocess-DS-5010/'
-                                     'intelliprocess/Testing_Datasets/iris.csv')))
+                          'pythonpackage-intelliprocess-DS-5010/'
+                          'intelliprocess/Testing_Datasets/iris.csv')))
+    # Works
+    print(outlier_removal_IQR_method(select_num('/Users/taylorgoodwin/Documents/GitHub/'
+                                                'pythonpackage-intelliprocess-DS-5010/'
+                                                'intelliprocess/Testing_Datasets/iris.csv')))
+    # Works
     print(histogram_plots(select_num('/Users/taylorgoodwin/Documents/GitHub/'
                                      'pythonpackage-intelliprocess-DS-5010/'
                                      'intelliprocess/Testing_Datasets/iris.csv')))
+
