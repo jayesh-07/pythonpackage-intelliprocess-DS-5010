8f2d81b9392b47a0e16296e4d095a0d8b7e4bb58
taygoodwin <124896968+taygoodwin@users.noreply.github.com>
2023-04-22 16:18:44 -0400

    Scaling, Skewness, Outliers, Some Plots
    histograms, log scaling, box-cox scaling, etc.

--- intelliprocess/scaling_outliers_skewness.py
+++ intelliprocess/scaling_outliers_skewness.py
@@ -4,19 +4,19 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy import stats
 import csv
-import math
 import os
+import datetime
 
 
-def column_names(data):
+def column_list(data):
     """
     Creates a list of column names for a given dataset.
     :param data: a pandas data frame
     :return:a list of the data's column names
     """
     columns_names = data.columns
-    columns_names = columns_names.tolist()
-    return columns_names
+    column_names_list = columns_names.tolist()
+    return column_names_list
 
 
 def select_num(data_location):
@@ -37,23 +37,17 @@ def select_num(data_location):
 
 def select_x(data_location, x_name):
     """
-    Creates a subset of a single variable in a
-    given dataset, when provided the datasets
+    Creates a subset containing a single variable in a
+    given dataset, when provided the dataset's
     filepath and column name of the chosen variable.
-    :param data_location:
+    :param data_location: filepath
     :param x_name: string indicating the name of
     the requested column to subset.
-    :return:
+    :return:data frame of data subset
     """
-    df_input = pd.read_csv(data_location)
-    data = pd.DataFrame(df_input)
-    for i in range(len(column_names(data))):
-        if column_names(data)[i] == x_name:
-            data_x = data.values[i][0:len(data.values[i]) - 1]
-            data_x = pd.DataFrame(data_x)
-            return data_x
-        else:
-            print('Specified column not found')
+    data = select_num(data_location)
+    df = data[x_name].copy().to_numpy()
+    return df
 
 
 # Skewness and Histogram PLots:
@@ -69,7 +63,7 @@ def skew(data_numeric):
     as values.
     """
     skewness = dict()
-    data_cols = column_names(data_numeric)
+    data_cols = column_list(data_numeric)
     for i in range(len(data_cols)):
         means = data_numeric.mean()
         mean = means.values
@@ -109,42 +103,49 @@ def histogram_plots(data_numeric):
 # Scaling/ Transforming Data:
 # Progressing - Need to check once 'select_x' fcn is complete
 def scaling_box_cox(x_column):
-    data_x = x_column.values
-    data_x = data_x.flatten()
+    """
 
-    print(data_x)
-    transformed, fitted_lambda = stats.boxcox(data_x)
+    :param x_column:
+    :return: None
+    """
+    transformed, fitted_lambda = stats.boxcox(x_column)
+    fig, axes = plt.subplots(1, 2)
 
-    for i in range(len(transformed)):
-        fig, axes = plt.subplots(1, 2)
-
-        sns.displot(data_x, hst=False, kde=True,
-                    kde_kws={'shade': True, 'linewidth': 2},
-                    label="Original Data", color="black", ax=axes[0])
-        sns.displot(transformed, hst=False, kde=True,
-                    kde_kws={'shade': True, 'linewidth': 2},
-                    label="Normal", color="black", ax=axes[1])
-
-    fig.set_figheight(5)
-    fig.set_figwidth(10)
+    sns.histplot(x_column, kde = True, ax = axes[0],
+                    label="Original Data", color="black")
+    sns.histplot(values,kind = "hist", kde = True, ax = axes[1],
+                    label ="Transformed Data", color="black")
     print(f"Lambda value used for Transformation: {fitted_lambda}")
-    return fig
+    plt.show()
+    return transformed
 
 
 # Log Transformation for a Given Column
 # Progressing - Incomplete
 # incorporate with scatterplot fcn (shane)
-def scaling_log(data_numeric):
-    for i in range(len(data_numeric)):
-        values = [data_numeric.values][i]
-        print(values)
-        # data_output = construct_df(values)
-        return values
+def scaling_log(x_column):
+    """
+    Transforms a single column of values based on
+    a log base 10 and provides two plots
+    :param x_column: a subset of a single variable
+    from a given dataset.
+    :return: data_output:
+    """
+    values = np.log10(x_column)
+    data_output = pd.DataFrame(values)
+    fig, axes = plt.subplots(1, 2)
+
+    sns.histplot(x_column, kde = True, ax = axes[0],
+                    label="Original Data", color="black")
+    sns.histplot(values,kind = "hist", kde = True, ax = axes[1],
+                    label ="Transformed Data", color="black")
+    plt.show()
+    return data_output
 
 
 # Outlier Detection and Removal
 # Enables IQR method
-def outlier_removal_IQR_method(data_numeric):
+def outlier_removal_IQR_method(data_numeric, save = True, filename = "intelliprocess_outliers_iqr_file"):
     """
     Removes outliers based on inter quartile range
     and provides a csv file output.
@@ -159,25 +160,33 @@ def outlier_removal_IQR_method(data_numeric):
     updated_data = data_numeric[~(
             (data_numeric < (Q1 - 1.5 * IQR)) | (data_numeric > (Q3 + 1.5 * IQR)))]
 
-    updated_data.to_csv('data_removed.csv')
+    if save is True:
+        timestamp = str(datetime.datetime.now()).replace(" ", "")
+        filename = filename + "" + timestamp + ".csv "
+        updated_data.to_csv(filename)
     return updated_data
 
 
 if __name__ == "__main__":
-    # Progressing
+    # Works
     print(scaling_box_cox(select_x('/Users/taylorgoodwin/Documents/GitHub/'
                                    'pythonpackage-intelliprocess-DS-5010/'
                                    'intelliprocess/Testing_Datasets/iris.csv', 'sepal_width')))
+
+    # Works
+    print(scaling_log(select_x('/Users/taylorgoodwin/Documents/GitHub/'
+                          'pythonpackage-intelliprocess-DS-5010/'
+                          'intelliprocess/Testing_Datasets/iris.csv', 'sepal_width')))
+
     # Works
     print(skew(select_num('/Users/taylorgoodwin/Documents/GitHub/'
                           'pythonpackage-intelliprocess-DS-5010/'
                           'intelliprocess/Testing_Datasets/iris.csv')))
     # Works
-    print(outlier_removal_IQR_method(select_num('/Users/taylorgoodwin/Documents/GitHub/'
+    outlier_removal_IQR_method(select_num('/Users/taylorgoodwin/Documents/GitHub/'
                                                 'pythonpackage-intelliprocess-DS-5010/'
-                                                'intelliprocess/Testing_Datasets/iris.csv')))
+                                                'intelliprocess/Testing_Datasets/iris.csv'))
     # Works
     print(histogram_plots(select_num('/Users/taylorgoodwin/Documents/GitHub/'
                                      'pythonpackage-intelliprocess-DS-5010/'
                                      'intelliprocess/Testing_Datasets/iris.csv')))
-
